a5f6756c66a62e8ab6a9ca0d1c6428debaa95c9e
Jomin Zhang <90665454+Birdaroof@users.noreply.github.com>
2021-10-05 06:46:56 -0400

    Add files via upload

---
+++ 10_BigSib/BigSib.java
@@ -0,0 +1,15 @@
+/*
+Zooming Xippy Zookeepers; Chompsky, Duck, Mr.Swag
+APCS
+HW 10- What Are Big Sibs Good For?
+*/
+public class BigSib {
+	public static void main(String[] args) {
+		greet("Anthony");
+		greet("Kevin");
+		greet("Jomin");
+	}
+	public static String greet(String str){
+		return("Why, hello there, " + str + ". How do you do?" );
+	}
+}
---
+++ 10_BigSib/Greet.java
@@ -0,0 +1,18 @@
+/*
+Zooming Xippy Zookeepers; Chompsky, Duck, Mr.Swag
+APCS
+HW 10 - What Are Big Sibs Good For?
+*/
+public class Greet {
+	public static void main(String[] args) {
+		System.out.println(BigSib.greet("Anthony"));
+		System.out.println(BigSib.greet("Kevin"));
+		System.out.println(BigSib.greet("Jomin"));
+	}
+}
+
+/*
+Notes
+- Changing the void to String allows the greet function to return a string.
+- You can then use system.out.println to print what the function returns which will be the string
+*/
