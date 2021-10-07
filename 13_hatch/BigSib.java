5e4e1676e39542b6c58ca0cc44c9688615120d05
Jomin <jzhang23@stuy.edu>
2021-10-07 00:35:12 -0400

    init setup

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,27 @@
+/*
+Zooming Xippy Zookeepers; Chompsky, Duck, Mr.Swag
+APCS
+HW 13- What Are Big Sibs Good For?
+Discoveries
+- Constructors do not have a return value
+- Constructors make it so that each new instance of the class BigSib will have the same default HelloMsg.
+- However this default value can still be changed and be different for each different object.
+-
+*/
+public class BigSib {
+	String setHelloMsg;
+	public BigSib(){
+		setHelloMsg = "Hello";
+	}
+	public void hold(String str){
+		String setHelloMsg = str;
+	}
+	public String greet(String str){
+	  return (setHelloMsg + " " + str);
+	}
+/*	public String setHelloMsg(String str){
+		setHelloMsg = str;
+		return setHelloMsg;
+	}
+*/
+}
