e681b1e28fa1b78f6518fc3c67508137fd226140
Jomin Zhang <90665454+Birdaroof@users.noreply.github.com>
2021-11-01 00:46:13 -0400

    Update helloName.java

--- cb/string-1/helloName.java
+++ cb/string-1/helloName.java
@@ -1,3 +1,19 @@
-public String helloName(String name) {
-  return "Hello " + name + "!";
+/**
+Jomin Zhang
+APCS
+HW28 -- Coding Bat
+2020-10-30
+
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+**/
+public class helloName{
+  public String helloName(String name) {
+    return "Hello " + name + "!";
+  }
+  public static void main(String[] args) {
+  System.out.println(helloName("Bob")); // → "Hello Bob!"
+  System.out.println(helloName("Alice")); // → "Hello Alice!"
+  System.out.println(helloName("X")); // → "Hello X!"
+  }
 }
