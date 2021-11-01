b9407915a05d94627e470b4f72c249def09d5fde
Jomin Zhang <90665454+Birdaroof@users.noreply.github.com>
2021-11-01 00:47:57 -0400

    Update makeAbba.java

--- cb/string-1/makeAbba.java
+++ cb/string-1/makeAbba.java
@@ -1,3 +1,20 @@
-public String makeAbba(String a, String b) {
-  return a + b + b + a;
+/*
+Jomin Zhang
+APCS
+HW28 -- Coding Bat
+2020-10-30
+*/
+
+/*
+Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+*/
+public class makeAbba.java{
+  public String makeAbba(String a, String b) {
+    return a + b + b + a;
+  }
+  public static void main(String[] args){
+    System.out.println(makeAbba("Hi", "Bye")); // → "HiByeByeHi"
+    System.out.println(makeAbba("Yo", "Alice")); // → "YoAliceAliceYo"
+    System.out.println(makeAbba("What", "Up")); // → "WhatUpUpWhat")
+  }
 }
