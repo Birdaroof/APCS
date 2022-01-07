50d834636718e074a4804e69ed797b2682707512
Jomin Zhang <90665454+Birdaroof@users.noreply.github.com>
2022-01-06 20:48:08 -0500

    Update InsertionSort.java

--- 53_InsertionSort/InsertionSort.java
+++ 53_InsertionSort/InsertionSort.java
@@ -72,7 +72,10 @@ public class InsertionSort
 
         // "walk" the current item to where it belongs
         // by swapping adjacent items
-        if (data.get(i).compareTo(data.get(i-1)) <0  ) {
+        if (i == data.size()){
+          break;
+        }
+        else if (data.get(i).compareTo(data.get(i-1)) <0  ) {
 
           System.out.println( "swap indices "+(i-1)+" & "+i+"..." ); //diag
           Comparable temp = data.get(i);
