2d249ff7f4bb78753dc9a32ecfad969111187d2f
Jomin Zhang <90665454+Birdaroof@users.noreply.github.com>
2022-03-08 09:05:09 -0500

    Update Mysterion.java

--- 71_unmask/Mysterion.java
+++ 71_unmask/Mysterion.java
@@ -7,9 +7,18 @@ public class Mysterion {
         swap(arr, c, b);
         var s = a;
         for (int i = a; i < (b - 1); i++) {
+            System.out.println("v = " + v);
             if (arr[i] < v) {
+                System.out.println("arr[i] = " + arr[i]);
+                System.out.println("v = " + v);
+                System.out.println("s = " + s);
+                System.out.println("arr[s] = " + arr[s]);
+                System.out.println("Entering the swap");
                 swap(arr, s, i);
                 s += 1;
+                System.out.println("Exiting the swap");
+                System.out.println("s = " + s);
+                System.out.println("arr[s] = " + arr[s]);
             }
         }
         swap(arr, b, s);
@@ -17,10 +26,13 @@ public class Mysterion {
     }
 
     public static int[] swap(int[] arr, int a, int b) {
+        System.out.println("Array before swap: " + print(arr));
+        System.out.println("Swapping " + arr[a] + " and "  + arr[b]);
         int place = arr[a];
         int[] holder = arr;
         holder[a] = arr[b];
         holder[b] = place;
+        System.out.println("Array after swap: " + print(holder));
         return holder;
     }
 
@@ -29,12 +41,13 @@ public class Mysterion {
         for (int i : arr ) {
             holder +=  i + ", ";
         }
+        holder += "}";
         return holder;
     }
 
     public static void main(String[] args) {
-        int[] ting = {7, 1, 5, 12, 3};
-        doTing(ting, 0, 4, 2);
+        int[] ting = {7, 1, 5, 4, 3};
+        doTing(ting, 0, 4, 3);
         System.out.println(print(ting));
     }
 }
